7644de542f3ab1c35811f6bb053f9fcba5354a74
hira0448 <99321564+hira0448@users.noreply.github.com>
2022-06-07 06:46:28 +0900

    ユーザー一覧画面の作成 ChakraUiのWrapを使用し、レスポンシブなユーザー一覧を作成

--- src/components/pages/UserManegement.tsx
+++ src/components/pages/UserManegement.tsx
@@ -1,5 +1,35 @@
+import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap>
+      <WrapItem>
+        <Box
+          w="260px"
+          h="260px"
+          bg="white"
+          borderRadius="10px"
+          shadow="md"
+          p={4}
+        >
+          <Stack textAlign="center">
+            <Image
+              borderRadius="full"
+              boxSize="160px"
+              src="https://source.unsplash.com/random"
+              alt="プロフィール画像"
+              m="auto"
+            />
+            <Text fontSize="lg" fontWeight="bold">
+              ひらの
+            </Text>
+            <Text fontSize="sm" color="gray">
+              Taiki Hirano
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
+    </Wrap>
+  );
 });
