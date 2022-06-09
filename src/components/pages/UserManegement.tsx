c53e841e27bcf1f01d46c03c75cc1c01116b6e50
hira0448 <99321564+hira0448@users.noreply.github.com>
2022-06-09 23:55:36 +0900

    ユーザー一覧画面の作成 ユーザー情報を格納しているBox部分をコンポーネント化

--- src/components/pages/UserManegement.tsx
+++ src/components/pages/UserManegement.tsx
@@ -1,35 +1,17 @@
-import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
     <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <Box
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-          p={4}
-          _hover={{ cursor: "pointer", opacity: 0.8 }}
-        >
-          <Stack textAlign="center">
-            <Image
-              borderRadius="full"
-              boxSize="160px"
-              src="https://source.unsplash.com/random"
-              alt="プロフィール画像"
-              m="auto"
-            />
-            <Text fontSize="lg" fontWeight="bold">
-              ひらの
-            </Text>
-            <Text fontSize="sm" color="gray">
-              Taiki Hirano
-            </Text>
-          </Stack>
-        </Box>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="ひらの"
+          fullName="Taiki Hirano"
+        />
       </WrapItem>
     </Wrap>
   );
