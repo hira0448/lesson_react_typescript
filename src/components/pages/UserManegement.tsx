d2123cb14b4a3f11f5f8e119f96398002d4ecc3d
hira0448 <99321564+hira0448@users.noreply.github.com>
2022-06-11 23:00:31 +0900

    ユーザー一覧取得機能の実装 hooksに作成したuseAllUsersをUserManegement.tsxへセット

--- src/components/pages/UserManegement.tsx
+++ src/components/pages/UserManegement.tsx
@@ -1,9 +1,16 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { memo, useEffect, VFC } from "react";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
   return (
     <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
