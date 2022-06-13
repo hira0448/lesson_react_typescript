1613f39e498e058a4d6edcd75fe746539948c93c
hira0448 <99321564+hira0448@users.noreply.github.com>
2022-06-13 23:52:03 +0900

    ユーザー詳細モーダル画面の作成 ユーザー詳細情報のモーダルを作成

--- src/components/pages/UserManegement.tsx
+++ src/components/pages/UserManegement.tsx
@@ -2,10 +2,17 @@
 
 import {
   Center,
+  FormControl,
+  FormLabel,
+  Input,
   Modal,
+  ModalBody,
+  ModalCloseButton,
   ModalContent,
+  ModalHeader,
   ModalOverlay,
   Spinner,
+  Stack,
   useDisclosure,
   Wrap,
   WrapItem
@@ -43,10 +50,36 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        autoFocus={false}
+        motionPreset="slideInBottom"
+      >
         <ModalOverlay />
-        <ModalContent>
-          <p>テスト</p>
+        <ModalContent pb={6}>
+          <ModalHeader>ユーザー詳細</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody mx={4}>
+            <Stack spacing={4}>
+              <FormControl>
+                <FormLabel>名前</FormLabel>
+                <Input value="ひらの　" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>フルネーム</FormLabel>
+                <Input value="Taiki Hirano　" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>MAIL</FormLabel>
+                <Input value="1234@example.com　" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>TEL</FormLabel>
+                <Input value="090-123-456　" isReadOnly />
+              </FormControl>
+            </Stack>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
